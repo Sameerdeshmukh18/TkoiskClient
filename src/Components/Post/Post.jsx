f62a1496308366724c961895e55fb8f3991f591a
Himanshu495-rada <himanshuyt20@gmail.com>
2023-12-12 11:21:10 +0530

    Added comment modal

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -5,7 +5,7 @@ import 'react-loading-skeleton/dist/skeleton.css'
 import verified_sign from "../../Assets/verify.png"
 import MoreActions from '../MoreActions/MoreActions'
 import { likeTweet, dislikeTweet } from '../../Services/TweetService'
-import { getUserDetails } from '../../Services/UserService'
+import { Modal, Button } from 'react-bootstrap';
 
 
 function Post(props) {
@@ -20,44 +20,49 @@ function Post(props) {
         "username": "username",
         "isVerified": false
     });
+    const [replyText, setReplyText] = useState('');
+    const [showModal, setShowModal] = useState(false);
+    const handleShow = () => setShowModal(true);
+    const handleClose = () => setShowModal(false);
 
     const togglePopup = () => {
         setPopupOpen(!isPopupOpen);
     };
 
     const like_Tweet = async (id) => {
-
-        setisLiked(true);
         const response = await likeTweet(id);
         if (response.ok) {
             setlikeCount(likeCount + 1);
+            setisLiked(true);
             console.log(response);
         }
     }
 
     const disLike_Tweet = async (id) => {
-        setisLiked(false);
         const response = await dislikeTweet(id);
         if (response.ok) {
             setlikeCount(likeCount - 1);
+            setisLiked(false);
             console.log(response);
         }
 
     }
 
+    const handleComment = () => {
+        console.log("Comment");
+    }
+
     useEffect(() => {
         setlikeCount(data.liked_by.length);
         setUser(data.user);
-        console.log(data.user);
     }, [data])
 
-    
+
     return (
         <div className='post'>
 
             <div className="profile-pic-icon">
                 <img src={profilepic} alt="Profile" className="profile-pic" />
-
 
             </div>
             <div className="post-info">
@@ -98,10 +103,65 @@ function Post(props) {
                         <div className="like action-icon" onClick={() => like_Tweet(data._id)}  > <i className="bi bi-heart" ></i> {likeCount}</div>
                     }
 
-                    <div className="share action-icon"><i className="bi bi-chat"></i></div>
-                    <div className="comment action-icon"><i className="bi bi-share"></i></div>
+                    <div className="comment action-icon" onClick={handleShow} ><i className="bi bi-chat"></i></div>
+                    <div className="share action-icon"><i className="bi bi-share"></i></div>
+
                 </div>
             </div>
+            <Modal show={showModal} onHide={handleClose}>
+                <Modal.Header closeButton className="border-0" style={{ marginRight: 'auto' }} />
+                <Modal.Body >
+                    <div className='post border border-1 border-dark'>
+                        <div className="profile-pic-icon">
+                            <img src={profilepic} alt="Profile" className="profile-pic" />
+
+                        </div>
+                        <div className="post-info">
+                            <div className="post-profile-info">
+                                <div className="username-verfied-box">
+                                    <div className="creator-name">{user.name}</div>
+                                    {user.isVerified
+                                        ?
+                                        <div className="verified"><img src={verified_sign} alt="" className="verified-sign-img" /></div>
+                                        :
+                                        null
+                                    }
+                                    <div className="creator-username">@{user.username}</div>
+                                </div>
+
+
+                                <div className="more-icon" onClick={togglePopup}>
+                                    <i className="bi bi-three-dots"></i>
+                                </div>
+
+                                {isPopupOpen && (
+                                    <MoreActions />
+                                )}
+
+                            </div>
+                            <div className="main-content">
+                                <p>
+                                    {data.tweet_text}
+                                </p>
+
+                            </div>
+                        </div>
+                    </div>
+                    <p className='mt-3' >Replying to @{user.username}</p>
+                    <textarea
+                        className='border mt-2'
+                        placeholder="Post your reply"
+                        value={replyText}
+                        onChange={(e) => setReplyText(e.target.value)}
+                        style={{ width: '100%', minHeight: '100px' }}
+                    />
+                </Modal.Body>
+                <Modal.Footer className="border-0" >
+                    <Button variant="primary" className="ml-auto rounded-pill" style={{ width: '100px', fontSize: '20px' }} onClick={handleComment}>
+                        Post
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
