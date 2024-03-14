4316b23f4f922b5f2dbcb2cea00d02cad81805d7
ishwar2303 <ib4717@srmist.edu.in>
2024-03-14 17:03:25 +0530

    Logic to Update Profile & Cover Photo

--- src/Components/ProfileSection/ProfileSection.jsx
+++ src/Components/ProfileSection/ProfileSection.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import './ProfileSection.css';
 import client from '../../apolloClient';
 import { useQuery, useMutation, gql } from '@apollo/client'
-import profilepic from "../../Assets/profilepic.jpg"
 import ApolloAPI from "../../ApiCllient";
 import InfiniteScroll from 'react-infinite-scroll-component'
 import Post from "../Post/Post";
@@ -10,14 +9,34 @@ import PostSkeleton from "../Post/PostSkeleton";
 
 function ProfileSection() {
 
-    const [userId, setUserId] = useState(localStorage.getItem('user_id'))
     const imageBaseURL = 'https://tkiosk-users-data.s3.ap-south-1.amazonaws.com'
-    const [coverImageURL, setCoverImageURL] = useState(`${imageBaseURL}/${userId}/CoverPhoto`)
-    const [selectedFile, setSelectedFile] = useState(null);
+    const [userId, setUserId] = useState(localStorage.getItem('user_id'))
+    const [selectedProfilePhotoFile, setSelectedProfilePhotoFile] = useState(null)
+    const [selectedCoverPhotoFile, setSelectedCoverPhotoFile] = useState(null)
+    const COVER_PHOTO_KEY = 'CoverPhoto'
+    const PROFILE_PHOTO_KEY = 'ProfilePhoto'
+
     const [myPostList, setMyPostList] = useState([]);
     const [cursor, setCursor] = useState();
     const ITEMS_PER_PAGE = 10;
     const hasMoreDataToLoad = cursor == "END" ? false : true
+
+    // This function definition should be above state variable 'coverImageVersion'
+    const getCoverImageLatestVersionToAvoidCacheIssue = () => {
+        const coverImageVersionStored = localStorage.getItem('CoverPhotoVersion')
+        return coverImageVersionStored ? parseInt(coverImageVersionStored) : 1
+    }
+    const [coverImageVersion, setCoverImageVersion] = useState(getCoverImageLatestVersionToAvoidCacheIssue())
+    const [coverImageURL, setCoverImageURL] = useState(`${imageBaseURL}/${userId}/${COVER_PHOTO_KEY}`)
+
+    // This function definition should be above state variable 'profileImageVersion'
+    const getProfileImageLatestVersionToAvoidCacheIssue = () => {
+        const profileImageVersionStored = localStorage.getItem('ProfilePhotoVersion')
+        return profileImageVersionStored ? parseInt(profileImageVersionStored) : 1
+    }
+    const [profileImageVersion, setProfileImageVersion] = useState(getProfileImageLatestVersionToAvoidCacheIssue())
+    const [profileImageURL, setProfileImageURL] = useState(`${imageBaseURL}/${userId}/${PROFILE_PHOTO_KEY}`)
+
 
     const QUERY_TO_GET_PRESIGNED_URL = gql`
     mutation Mutation($fileName: String!, $fileType: String!) {
@@ -79,7 +98,6 @@ function ProfileSection() {
     const [userDetails, setUserDetails] = useState({ name: '', username: '', followers: [], followersCount: 0, following: [], followingCount: 0, dob: '', bio: '' });
 
     const fetchMoreData = () => {
-        console.log("inside fetch more data")
         setTimeout(() => {
             fetchMore({
                 variables: {
@@ -91,10 +109,7 @@ function ProfileSection() {
                 }
                 setMyPostList(myPostList.concat(res.data.getMyTweets.tweets));
                 setCursor(res.data.getMyTweets.endCursor);
-                console.log(`Cursor: ${cursor}`)
             }).catch((err) => {
-                console.log('ERROR -x-x-x-x-x-x-x-x')
-                console.log(err)
             })
         }, 1500)
     }
@@ -146,41 +161,55 @@ function ProfileSection() {
         return `${userId}/${imageForDescription}`
     }
 
-    /*
-    Invokes when file is selected and updates the state variable to hold the selected file
-    */
-    const handleFileChange = (event) => {
+    // Handles when file is selected for cover photo
+    const handleCoverFileChange = (event) => {
         const file = event.target.files[0]
-        setSelectedFile(file)
+        setSelectedCoverPhotoFile(file)
     };
 
+    // Handles when file is selected for profile photo
+    const handleProfileFileChange = (event) => {
+        const file = event.target.files[0]
+        setSelectedProfilePhotoFile(file)
+    }
+
+    // Update the state variable to hold selected cover photo file and give to upload function
+    const handleCoverPhotoUpload = () => {
+        uploadToS3Bucket(selectedCoverPhotoFile, COVER_PHOTO_KEY)
+    }
+    // Update the state variable to hold selected profile photo file and give to upload function
+    const handleProfilePhotoUpload = () => {
+        uploadToS3Bucket(selectedProfilePhotoFile, PROFILE_PHOTO_KEY)
+    }
 
     // Uploads the photo to s3 bucket
-    const handleUpload = async () => {
-        if (!selectedFile) return
-        const fileName = getFileName('CoverPhoto')
-        getPresignedURL(fileName, selectedFile.type)
+    const uploadToS3Bucket = (file, fileForDescription) => {
+        if (!file) return
+        const fileName = getFileName(fileForDescription)
+        getPresignedURL(fileName, file.type)
             .then((presignedURL) => {
                 // Upload the file to S3 using the pre-signed URL
                 fetch(presignedURL, {
                     method: 'PUT',
-                    body: selectedFile,
+                    body: file,
                     headers: {
-                        'Content-Type': selectedFile.type,
+                        'Content-Type': file.type,
                     }
                 })
                     .then((res) => {
                         console.log('File Uploaded Successfully :)')
 
-                        // only for testing purpose
-                        // let a = document.createElement('a')
-                        // a.target = '_blank'
-                        // a.href = `${imageBaseURL}/${fileName}`
-                        // a.click()
+                        if(fileForDescription == COVER_PHOTO_KEY) {
+                            setCoverImageVersion(coverImageVersion+1)
+                            setSelectedCoverPhotoFile(null)
+                            localStorage.setItem('CoverPhotoVersion', coverImageVersion)
+                        }
 
-                        // Reset state variables after file upload
-                        setSelectedFile(null)
-                        window.location.reload();
+                        if(fileForDescription == PROFILE_PHOTO_KEY) {
+                            setProfileImageVersion(profileImageVersion+1)
+                            setSelectedProfilePhotoFile(null)
+                            localStorage.setItem('ProfilePhotoVersion', profileImageVersion)
+                        }
                     })
                     .catch((err) => {
                         console.log(err)
@@ -210,10 +239,10 @@ function ProfileSection() {
         <div className='ProfileSection' id='profileSection'>
             <div className="profile-details">
                 <div className="cover-picture">
-                    <img src={coverImageURL} alt="Cover Photo" />
+                    <img src={`${coverImageURL}?v=${coverImageVersion}`} alt="Cover Photo" />
                 </div>
                 <div className="profile-picture">
-                    <img src={profilepic} alt="Profile Photo" />
+                <img src={`${profileImageURL}?v=${profileImageVersion}`} alt="Profile Photo" />
                 </div>
                 <div className="profile-card">
                     <div className="username-name">
@@ -222,8 +251,11 @@ function ProfileSection() {
                     </div>
 
                     {/* Below code is for testing the upload image functionality */}
-                    <input type="file" onChange={handleFileChange} />
-                    <button onClick={handleUpload}>Upload Image</button>
+                    <input type="file" onChange={handleCoverFileChange} />
+                    <button onClick={handleCoverPhotoUpload} className='upload-btn'>Update Cover Photo</button>
+                    <hr />
+                    <input type="file" onChange={handleProfileFileChange} />
+                    <button onClick={handleProfilePhotoUpload} className='upload-btn'>Update Profile Photo</button>
 
                     <div className="profile-bio">{userDetails.bio}</div>
                     <div className="connections-count">
